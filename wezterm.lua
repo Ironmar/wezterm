f37e83290a19fd2a6fcf752da7e0086174172aa2
Ironmar <fmrm93@gmail.com>
2023-11-27 09:11:59 -0500

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(unix or {})
+	window:gui_window():maximize()
+end)
+
+config.color_scheme = "OneHalfDark"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 5,
+	right = 5,
+	top = 20,
+	bottom = 0,
+}
+
+return config
